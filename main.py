5b8964c8a8da1bb92752d79f8e98d9eab638f04b
skvadda1 <156102921+skvadda1@users.noreply.github.com>
2024-10-26 17:14:19 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+def add(x,y):
+    x + y
+
+
+def __init__(self, num1, num2, num3):
+    num1 = int(input("What is the 1st number?"))
+    num2 = int(input("What is the 2nd number?"))
+    num3 = int(input("What is the 3rd number?"))
+    print("Sum Is:" , add(num1, num2, num3))
+
+
+ 
